8f9c2af7b02d77ae48088831c4ba3aa25039c1c0
Ben Asher <benasher44@gmail.com>
2019-12-20 12:02:57 -0500

    docs

--- src/nativeMain/kotlin/com/autodesk/coroutineworker/AtomicBoolean.kt
+++ src/nativeMain/kotlin/com/autodesk/coroutineworker/AtomicBoolean.kt
@@ -3,8 +3,14 @@ package com.autodesk.coroutineworker
 import kotlin.native.concurrent.AtomicInt
 import kotlin.native.concurrent.freeze
 
+/**
+ * Boolean wrapper around AtomicInt
+ */
 internal class AtomicBoolean(value: Boolean) {
 
+    /**
+     * Backing store for the Boolean value
+     */
     private val _value: AtomicInt
 
     init {
@@ -12,6 +18,9 @@ internal class AtomicBoolean(value: Boolean) {
         freeze()
     }
 
+    /**
+     * Public API for the wrapped Boolean, which represents the Boolean value
+     */
     var value: Boolean
         get() = _value.value == 1
         set(value) {
