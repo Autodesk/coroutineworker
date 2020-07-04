4e29f173d9da752db80086f6211c73fb75a86313
Ahmed Elkhoudiry <ahmedelkhodery2@gmail.com>
2020-07-04 23:47:11 +0200

    Add JavaScript support (#55)

---
+++ src/jsMain/kotlin/com/autodesk/coroutineworker/CoroutineWorker.kt
@@ -0,0 +1,40 @@
+package com.autodesk.coroutineworker
+
+import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.launch
+
+actual class CoroutineWorker internal actual constructor() : CoroutineScope {
+
+    private val job = Job()
+
+    /**
+     * The context of this scope. See [CoroutineScope.coroutineContext]
+     */
+    override val coroutineContext: CoroutineContext = job
+
+    actual fun cancel() {
+        job.cancel()
+    }
+
+    actual suspend fun cancelAndJoin() {
+        job.cancelAndJoin()
+    }
+
+    actual companion object {
+
+        actual fun execute(block: suspend CoroutineScope.() -> Unit): CoroutineWorker {
+            return CoroutineWorker().also {
+                it.launch(block = block)
+            }
+        }
+
+        actual suspend fun <T> withContext(jvmContext: CoroutineContext, block: suspend CoroutineScope.() -> T): T {
+            return kotlinx.coroutines.withContext(jvmContext) {
+                block()
+            }
+        }
+    }
+}
---
+++ src/jsTest/kotlin/com/autodesk/coroutineworker/Platform.kt
@@ -0,0 +1,10 @@
+package com.autodesk.coroutineworker
+
+import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.promise
+
+actual fun <T> runBlocking(context: CoroutineContext, block: suspend CoroutineScope.() -> T): dynamic = GlobalScope.promise { block() }
+
+actual fun Any.ensureNeverFrozen() = Unit
