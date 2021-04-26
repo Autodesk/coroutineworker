867b836d4459d0052e42183c0d999b152ff48c9c
Ben Asher <ben.asher@autodesk.com>
2021-04-26 16:43:54 -0700

    Upgrade to Kotlin 1.5.0 (#78)

--- src/jsTest/kotlin/com/autodesk/coroutineworker/Platform.kt
+++ src/jsTest/kotlin/com/autodesk/coroutineworker/Platform.kt
@@ -1,10 +1,12 @@
 package com.autodesk.coroutineworker
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.promise
 import kotlin.coroutines.CoroutineContext
 
+@OptIn(DelicateCoroutinesApi::class)
 actual fun <T> runBlocking(context: CoroutineContext, block: suspend CoroutineScope.() -> T): dynamic = GlobalScope.promise { block() }
 
 actual fun Any.ensureNeverFrozen() = Unit
